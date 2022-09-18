0c25c86a75c42a0f9ad31aa433aa0a15a472610a
Sabyasachi Choudhury <50119508+techSaby@users.noreply.github.com>
2022-09-18 19:24:57 +0530

    Delete SpringBoot-Cache-Service directory

--- SpringBoot-Cache-Service/src/main/java/com/techsaby/springbootcache/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techsaby.springbootcache.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.techsaby.springbootcache.model.Books;
-
-public interface BookRepository extends MongoRepository<Books, Long>{
-
-}
