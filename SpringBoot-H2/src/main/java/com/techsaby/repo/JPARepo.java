0e82f7fc9d0ac8f52efe8eb2f37a40da10c094e7
Sabyasachi Choudhury <50119508+techSaby@users.noreply.github.com>
2022-09-18 19:25:09 +0530

    Delete SpringBoot-H2 directory

--- SpringBoot-H2/src/main/java/com/techsaby/repo/JPARepo.java
+++
@@ -1,9 +0,0 @@
-package com.techsaby.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techsaby.model.SaveIdea;
-
-public interface JPARepo extends JpaRepository<SaveIdea, Long> {
-
-}
